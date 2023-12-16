acd6b92794d089afb91af16d63f6f3954607ef72
MarlonPasseri <marlonn.contato@gmail.com>
2023-12-16 01:06:52 -0300

    NovoCommit

---
+++ AT DE JAVA/Models/Restaurante.cs
@@ -0,0 +1,14 @@
+﻿namespace AT_DE_JAVA.Models
+{
+    public class Restaurante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Comida { get; set;}
+
+        public string Garçon { get; set;}
+
+        public string Ingrediente { get; set;}
+    }
+}
---
+++ AT DE JAVA/Models/Talheres.cs
@@ -0,0 +1,11 @@
+﻿namespace AT_DE_JAVA.Models
+{
+    public class Talheres
+    {
+        public int id {  get; set; }
+
+        public string nome { get; set; }
+
+        public double preço { get; set; }
+    }
+}
